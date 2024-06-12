51996393ed074994d41c712690dd513ed2392b58
syshin0116 <syshin0116@gmail.com>
2024-06-13 00:29:07 +0900

    vault backup: 2024-06-13 00:29:07

--- remove_control_code.py
+++ remove_control_code.py
@@ -2,20 +2,23 @@ import os
 
 
 def remove_prohibited_control_code(file_path):
-    with open(file_path, "r", encoding="utf-8", errors="ignore") as file:
-        content = file.read()
+    try:
+        with open(file_path, "r", encoding="utf-8", errors="ignore") as file:
+            content = file.read()
 
-    cleaned_content = content.replace("\x08", "")  # U+0008 제거
+        cleaned_content = content.replace("\x08", "")  # U+0008 제거
 
-    with open(file_path, "w", encoding="utf-8") as file:
-        file.write(cleaned_content)
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(cleaned_content)
 
-    print(f"Prohibited control code points removed from {file_path}")
+        print(f"Prohibited control code points removed from {file_path}")
+    except PermissionError:
+        print(f"Permission denied: {file_path}")
 
 
 # 포함할 디렉토리 목록 및 확장자 목록
-included_dirs = {"_posts", "_site", "_drafts"}
-included_extensions = {".html"}
+included_dirs = {"_posts", "_site"}
+included_extensions = {".html", ".md"}
 
 for root, dirs, files in os.walk("."):
     # 포함할 디렉토리만 탐색
