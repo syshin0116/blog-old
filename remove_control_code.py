55c80cddc43be7599d3b5333df8c21d67ad26bc4
syshin0116 <syshin0116@gmail.com>
2024-06-13 00:04:41 +0900

    fix: remove unwanted symbols automatically

--- remove_control_code.py
+++ remove_control_code.py
@@ -14,7 +14,10 @@ def remove_prohibited_control_code(file_path):
 
 
 # 전체 프로젝트 디렉토리에서 파일을 재귀적으로 검사하고 수정
+excluded_dirs = {"_posts", "_drafts", "_includes", "layouts"}
 for root, dirs, files in os.walk("."):
+    # 제외할 디렉토리 무시
+    dirs[:] = [d for d in dirs if d not in excluded_dirs and d != ".git"]
     for file in files:
         file_path = os.path.join(root, file)
         remove_prohibited_control_code(file_path)
