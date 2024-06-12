3863a1de7c13b10adc3dce0aa72cfbad983fca0e
syshin0116 <syshin0116@gmail.com>
2024-06-13 00:12:07 +0900

    fix: git action

--- remove_control_code.py
+++ remove_control_code.py
@@ -13,11 +13,13 @@ def remove_prohibited_control_code(file_path):
     print(f"Prohibited control code points removed from {file_path}")
 
 
-# 전체 프로젝트 디렉토리에서 파일을 재귀적으로 검사하고 수정
-excluded_dirs = {"_posts", "_drafts", "_includes", "layouts"}
+# 포함할 디렉토리 목록
+included_dirs = {"_posts"}
+
 for root, dirs, files in os.walk("."):
-    # 제외할 디렉토리 무시
-    dirs[:] = [d for d in dirs if d not in excluded_dirs and d != ".git"]
+    # 포함할 디렉토리만 탐색
+    dirs[:] = [d for d in dirs if d in included_dirs]
     for file in files:
         file_path = os.path.join(root, file)
-        remove_prohibited_control_code(file_path)
+        if any(dir in file_path for dir in included_dirs):
+            remove_prohibited_control_code(file_path)
